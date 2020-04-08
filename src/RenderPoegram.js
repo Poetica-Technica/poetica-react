8eac068a9fdf4898b0b134413f2d8f09fbc8f592
Nathan Martel <nathanmartel@gmail.com>
2020-04-07 21:29:32 -0700

    Add poegram css background

--- src/RenderPoegram.js
+++ src/RenderPoegram.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './poegram.css'
 
 export default class RenderPoegram extends Component {
   render() {
     return (
-      <div className="poegram">
+      <div className="poegram background-1">
         <p className="poem">{ this.props.poegram.poemId.lines }</p>
         <p className="author">— { this.props.poegram.poemId.author }, from <em>{ this.props.poegram.poemId.title }</em></p>
       </div>
