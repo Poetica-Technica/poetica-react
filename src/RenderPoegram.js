6061843e183f0a24410fc6777cae09a81571f0ed
Nathan Martel <nathanmartel@gmail.com>
2020-04-09 08:01:15 -0700

    Add style tag to pull poegram object's colors

--- src/RenderPoegram.js
+++ src/RenderPoegram.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 import './poegram.css'
 
 export default class RenderPoegram extends Component {
+  
   render() {
+
+    const divStyle = {
+      backgroundImage: `radial-gradient(circle at top right, ${this.props.poegram.colors[0]}, ${this.props.poegram.colors[1]})`
+    };
+
     return (
-      <div className="poegram background-1">
+      <div className="poegram background-1" style={divStyle}>
         <p className="poem">{ this.props.poegram.poemId.lines }</p>
         <p className="author">— { this.props.poegram.poemId.author }, from <em>{ this.props.poegram.poemId.title }</em></p>
       </div>
